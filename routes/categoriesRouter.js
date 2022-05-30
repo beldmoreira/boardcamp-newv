b7e7fba859ec296dc3184495e04a1370db4b7c2c
Isabela <isabela_cmoreira@hotmail.com>
2022-05-30 16:03:10 -0300

    feat: Categories route finished. Added a middleware

--- routes/categoriesRouter.js
+++ routes/categoriesRouter.js
@@ -1,9 +1,10 @@
 import { Router } from "express";
 import { getCategories,addCategory } from "../controllers/categoriesController.js";
+import  * as validation from "../middlewares/categoriesValidator.js";
 
 const categoriesRouter = Router();
 
-categoriesRouter.get("/categories", getCategories);
-categoriesRouter.post("/categories", addCategory);
+categoriesRouter.get("/categories",getCategories);
+categoriesRouter.post("/categories",validation.validateCategories,addCategory);
 
 export default categoriesRouter;
