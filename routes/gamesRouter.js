e88295c37b1121239f958f3b596f0eab6c7e5277
Isabela <isabela_cmoreira@hotmail.com>
2022-05-30 16:15:53 -0300

    feat: Added a middleware to the Game routes

--- routes/gamesRouter.js
+++ routes/gamesRouter.js
@@ -1,10 +1,11 @@
 import { Router } from "express";
 import {getGames,addGame} from "../controllers/gamesController.js"
+import * as validation from "../middlewares/gamesValidator.js"
 
 const gamesRouter = Router();
 
 gamesRouter.get("/games", getGames);
-gamesRouter.post("/games", addGame);
+gamesRouter.post("/games",validation.validateGames,addGame);
 
 export default gamesRouter;
 
