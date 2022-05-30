80e13c05164608dec1ec6d68ee896fa1edeee425
Isabela <isabela_cmoreira@hotmail.com>
2022-05-30 16:36:56 -0300

    feat: added a middleware to both routes

--- routes/costumersRouter.js
+++ routes/costumersRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
 import {getCostumers,addCostumers,updateCostumer,getCustomerId} from "../controllers/costumersController.js"
+import * as validation from "../middlewares/costumersValidator.js"
 
 const costumersRouter = Router();
 
 costumersRouter.get("/customers", getCostumers);
 costumersRouter.get("/customers/:id",getCustomerId);
-costumersRouter.post("/customers",addCostumers);
-costumersRouter.put("/customers/:id",updateCostumer);
+costumersRouter.post("/customers",validation.validateCostumers,addCostumers);
+costumersRouter.put("/customers/:id",validation.validateCostumers,updateCostumer);
 
 export default costumersRouter;
--- routes/rentalsRouter.js
+++ routes/rentalsRouter.js
@@ -1,11 +1,12 @@
 import { Router } from "express";
-import {getRentals,postRental,endRental,deleteRental} from "../controllers/rentalsController.js"
+import {getRentals,postRental,endRental,deleteRental} from "../controllers/rentalsController.js";
+import  * as validation from "../middlewares/rentalsValidator.js";
 
 const rentalsRouter = Router();
 
 rentalsRouter.get("/rentals", getRentals);
-rentalsRouter.post("/rentals", postRental);
-rentalsRouter.post("/rentals/:id/return",endRental);
-rentalsRouter.delete("/rentals/:id", deleteRental)
+rentalsRouter.post("/rentals", validation.validateRentals,postRental);
+rentalsRouter.post("/rentals/:id/return",validation.validateRentals,endRental);
+rentalsRouter.delete("/rentals/:id",validation.validateRentals,deleteRental)
 
 export default rentalsRouter;
